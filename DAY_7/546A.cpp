46f5979ce0497aac5fa5cea109e774320fccfcfe
Rida702 <ai22c0rida@gmail.com>
2023-01-05 23:52:31 +0500

    DAY 07

---
+++ DAY_7/546A.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int k, n, w,sum=0;
+	cin >> k >> n >> w;
+	for( int i=1; i<=w; i++)
+		sum+=i*k;
+	if (sum-n>0) cout << sum-n << endl;
+	else cout << 0 << endl;
+	return 0;
+}
