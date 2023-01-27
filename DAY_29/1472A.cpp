0eb60c4f4e27f092cf76a858c52b72a6cd6e7c84
Rida702 <ai22c0rida@gmail.com>
2023-01-27 23:46:22 +0500

    Codeday29commitsagain

--- DAY_29/1472A.cpp
+++ DAY_29/1472A.cpp
@@ -5,31 +5,22 @@ int main(){
 	int n;
 	cin >> n;
 	for( int run=0; run<n; run++){
-		int a,b,c,count=0;
+		int a,b,c,count=1;
 		cin >> a;
 		cin >> b;
 		cin >> c;
-		if ( c==1 || ( a==1 && b%2==0 && b>=c ) ) cout << "YES" << endl;
-		else if ( a%2!=0 && b%2!=0 )	cout << "NO" << endl;
+		if ( c==1 ) cout << "YES" << endl;
 		else {
-			while(true){
-			if( a%2!=0 && b%2!=0 ) break;
-			else if( a%2==0 && b%2==0) {
-				a=a/2;
-				b=b/2;
-				count+=4;
-			}
-			else if( a%2==0 ) {
-				count+=2;
-				a=a/2;
-			}
-			else if( b%2==0 ) {
-				count+=2;
-				b=b/2;
-			}
+		while(a%2==0){
+			count*=2;
+			a/=2;
 		}
-		if(count>=c ) cout << "YES" << endl;
-		else          cout << "NO" << endl;
+		while(b%2==0){
+			count*=2;
+			b/=2;
+		} 
+		if(count>=c) cout << "YES" << endl;
+		else cout << "NO" << endl;
 		}
 	}
 	return 0;
